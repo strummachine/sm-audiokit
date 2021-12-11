099f8513aca63854d268ee63780f7876412d5dc1
Luke Abbott <luke@playingbyear.com>
2021-12-11 10:52:52 -0800

    Sample player pool improvements

--- Sources/Audio/SamplePlayerPool.swift
+++ Sources/Audio/SamplePlayerPool.swift
@@ -15,13 +15,13 @@ class SamplePlayerPool {
 
     func getPlayer(forSample sample: Sample) -> SamplePlayer {
         let debugPreloadedCount = self.players.filter({ $0.available && $0.sampleId == sample.id }).count
-        print("Getting player for \(sample.id.padding(toLength: 15, withPad: " ", startingAt: 0)) - \(self.players.filter({ $0.available }).count) of \(self.size) available, \(debugPreloadedCount > 0 ? String(debugPreloadedCount) : "ZERO") preloaded")
+        print("[SamplePlayerPool] Players for \(sample.id.padding(toLength: 15, withPad: " ", startingAt: 0)) - \(self.players.filter({ $0.available }).count) of \(self.size) available, \(debugPreloadedCount > 0 ? String(debugPreloadedCount) : "ZERO") preloaded")
         let playerWithSampleLoaded = self.players.first(where: { $0.available && $0.sampleId == sample.id })
         if playerWithSampleLoaded != nil {
             return playerWithSampleLoaded!
         }
         let sortedPlayers = self.players.sorted { a, b in
-            return ((a.startTime?.hostTime ?? 0) < (b.startTime?.hostTime ?? 0))
+            return ((a.startTime?.hostTime ?? 0) < (b.startTime?.hostTime ?? 1))
         }
         return sortedPlayers.first(where: { $0.available && $0.sampleId == nil })
             ?? sortedPlayers.first(where: { $0.available })
@@ -29,8 +29,8 @@ class SamplePlayerPool {
     }
     
     public func stopAllPlayers() {
+        print("[SamplePlayerPool] Stopping all players")
         for player in players {
-            print("Stopping player:\(player.sampleId)")
             player.stopImmediately()
         }
     }
