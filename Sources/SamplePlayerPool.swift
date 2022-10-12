45c4315213a9df21640e4950b95d572e9c16dcd7
Luke Abbott <luke@playingbyear.com>
2022-10-12 10:59:48 -0700

    Pool cleanup

--- Sources/SamplePlayerPool.swift
+++ Sources/SamplePlayerPool.swift
@@ -2,15 +2,13 @@ import Foundation
 import AudioKit
 
 final class SamplePlayerPool {
-    var arrayLock = NSRecursiveLock()
-
-    static var globalArrayLock = NSRecursiveLock()
+    private static var opLock = NSRecursiveLock()
     static var globalPlayerDict = [String:SamplePlayer]()
 
     static func withPlayer(playbackId: String, _ code: @escaping (_ value: SamplePlayer) -> ()) {
-        globalArrayLock.lock()
+        opLock.lock()
         let player = globalPlayerDict[playbackId]
-        globalArrayLock.unlock()
+        opLock.unlock()
         if player != nil {
             code(player!)
         }
@@ -21,50 +19,56 @@ final class SamplePlayerPool {
     var playersAvailable = [SamplePlayer]()
 
     func createPlayers(count: Int) {
-        SamplePlayerPool.globalArrayLock.lock()
+        SamplePlayerPool.opLock.lock()
         for _ in 0..<count {
             let player = SamplePlayer(pool: self)
             self.players.append(player)
             self.playersAvailable.append(player)
         }
-        SamplePlayerPool.globalArrayLock.unlock()
+        SamplePlayerPool.opLock.unlock()
     }
     
     func debugLine() -> String {
-        SamplePlayerPool.globalArrayLock.lock()
-        let result = String(repeating: "X", count: self.playersInUse.count) + String(repeating: "-", count: self.playersAvailable.count)
-        SamplePlayerPool.globalArrayLock.unlock()
+        SamplePlayerPool.opLock.lock()
+        let result = String(players.count).padding(toLength: 3, withPad: " ", startingAt: 0) + " " + String(repeating: "X", count: self.playersInUse.values.filter({ $0.player.playerNode.isPlaying }).count) + String(repeating: "?", count: self.playersInUse.values.filter({ !$0.player.playerNode.isPlaying }).count) + String(repeating: "-", count: self.playersAvailable.count)
+        SamplePlayerPool.opLock.unlock()
         return result
     }
 
     func reservePlayer(playbackId: String) -> SamplePlayer? {
-        SamplePlayerPool.globalArrayLock.lock()
+        SamplePlayerPool.opLock.lock()
         let player = playersAvailable.popLast()
         if let availablePlayer = player {
             playersInUse[playbackId] = availablePlayer
-            SamplePlayerPool.globalPlayerDict[playbackId] = player
+            SamplePlayerPool.globalPlayerDict[playbackId] = availablePlayer
         }
-        SamplePlayerPool.globalArrayLock.unlock()
+        SamplePlayerPool.opLock.unlock()
         return player
     }
 
     func returnPlayer(_ player: SamplePlayer) {
-        SamplePlayerPool.globalArrayLock.lock()
+        SamplePlayerPool.opLock.lock()
         if let playbackId = player.playbackId {
             player.reset()
             playersInUse.removeValue(forKey: playbackId)
             SamplePlayerPool.globalPlayerDict.removeValue(forKey: playbackId)
+        } else {
+            print("⛔️ Tried to returnPlayer with no playbackId!")
+            print(player)
         }
         playersAvailable.insert(player, at: 0)
-        SamplePlayerPool.globalArrayLock.unlock()
+        SamplePlayerPool.opLock.unlock()
     }
     
     public func stopAllPlayers() {
-        SamplePlayerPool.globalArrayLock.lock()
+        SamplePlayerPool.opLock.lock()
         for player in players {
             player.stopImmediately()
         }
-        SamplePlayerPool.globalArrayLock.unlock()
+        playersInUse.removeAll()
+        SamplePlayerPool.globalPlayerDict.removeAll()
+        playersAvailable = players  // in Swift, this is a copy
+        SamplePlayerPool.opLock.unlock()
     }
 
     /*
